0080df05ecd9e7ea950aacbee4f78ae9147789b3
Fortunatus <wulnyefortunatus@gmail.com>
2023-09-29 08:06:06 +0000

    files added

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from typing import List
+
+def pascal_triangle(n: int) -> List[List[int]]:
+    """
+    Generate Pascal's triangle up to n rows.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for _ in range(1, n):
+        row = [1]
+        last_row = triangle[-1]
+        for j in range(len(last_row) - 1):
+            row.append(last_row[j] + last_row[j + 1])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
