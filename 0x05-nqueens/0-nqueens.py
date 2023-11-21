76b2371578efb111b457fb19fd9b87f73c7afbac
Fortunatus <wulnyefortunatus@gmail.com>
2023-11-21 22:07:22 +0000

    readme

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,57 +3,62 @@
 import sys
 
 
-def print_board(board, n):
-    """Print allocated positions to the queen"""
-    b = []
+def print_board(board):
+    """Format the board for printing"""
+    formatted_board = []
+    n = len(board)
 
     for i in range(n):
-        for j in range(n):
-            if j == board[i]:
-                b.append([i, j])
-    print(b)
+        formatted_board.append([i, board[i]])
+
+    return formatted_board
 
 
-def is_position_safe(board, i, j, r):
-    """Checks if the position is safe for the queen"""
-    return board[i] in (j, j - i + r, i - r + j)
+def is_position_safe(board, row, col):
+    """Check if the position is safe for the queen"""
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
 
 
-def safe_positions(board, row, n):
+def safe_positions(board, row, n, solutions):
     """Find all safe positions where the queen can be allocated"""
     if row == n:
-        print_board(board, n)
+        solutions.append(print_board(board))
+        return
 
-    else:
-        for j in range(n):
-            allowed = True
-            for i in range(row):
-                if is_position_safe(board, i, j, row):
-                    allowed = False
-            if allowed:
-                board[row] = j
-                safe_positions(board, row + 1, n)
+    for j in range(n):
+        if is_position_safe(board, row, j):
+            board[row] = j
+            safe_positions(board, row + 1, n, solutions)
 
 
-def create_board(size):
-    """Generates the board"""
-    return [0 * size for i in range(size)]
+def n_queens(n):
+    """Main function to solve the N Queens problem"""
+    solutions = []
+    board = [-1] * n
+    safe_positions(board, 0, n, solutions)
+    return solutions
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-try:
-    n = int(sys.argv[1])
-except BaseException:
-    print("N must be a number")
-    exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
 
-if (n < 4):
-    print("N must be at least 4")
-    exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-board = create_board(int(n))
-row = 0
-safe_positions(board, row, int(n))
+    solutions = n_queens(n)
+    for solution in solutions:
+        print(solution)
